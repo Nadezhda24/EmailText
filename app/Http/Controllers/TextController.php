9e9be5d74f7162c77edcfeb1f3be15da188462f7
Nadezhda24 <gerasimova.nadezhda2001@gmail.com>
2022-11-06 10:07:51 +0300

    form view

---
+++ app/Http/Controllers/TextController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TextController extends Controller{
+
+    public function submit(Request $request){
+        dd($request);
+    }
+
+}
